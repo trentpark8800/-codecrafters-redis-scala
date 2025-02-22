e5fb2f57d37db63620848af5d16b76b7eb04bc04
Trent Park <trentpark8800@gmail.com>
2025-02-22 19:43:21 +0200

    Hardcode pong response to any request

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -12,5 +12,9 @@ object Server {
     val serverSocket = new ServerSocket()
     serverSocket.bind(new InetSocketAddress("localhost", 6379))
     val clientSocket = serverSocket.accept() // wait for client
+
+    val dataIn = clientSocket.getInputStream
+    val dataOut = clientSocket.getOutputStream
+    dataOut.write("+PONG\r\n".getBytes)
   }
 }
