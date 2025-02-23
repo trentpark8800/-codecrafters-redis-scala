47ed18236d6fcd88f36c74e19168b32d7d465ed1
Trent Park <trentpark8800@gmail.com>
2025-02-23 15:08:35 +0200

    Handle multiple ping requests and respond with pong

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -1,6 +1,7 @@
 package codecrafters_redis
 
 import java.net.{InetSocketAddress, ServerSocket}
+import java.io.{BufferedReader, InputStreamReader, OutputStream}
 
 object Server {
   def main(args: Array[String]): Unit = {
@@ -11,10 +12,18 @@ object Server {
     
     val serverSocket = new ServerSocket()
     serverSocket.bind(new InetSocketAddress("localhost", 6379))
-    val clientSocket = serverSocket.accept() // wait for client
 
-    val dataIn = clientSocket.getInputStream
-    val dataOut = clientSocket.getOutputStream
-    dataOut.write("+PONG\r\n".getBytes)
+    while (true) {
+      val clientSocket = serverSocket.accept() // wait for clients
+      val inputReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream))
+      val outputStream = clientSocket.getOutputStream
+
+      var requestLine: String = null
+      while ({ requestLine = inputReader.readLine(); requestLine != null }) {
+        outputStream.write("+PONG\r\n".getBytes)
+        outputStream.flush()
+      }
+    }
+    
   }
 }
