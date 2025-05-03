2169b34f2095a0f7984d411a7779191d990cafad
Trent Park <trentpark8800@gmail.com>
2025-05-03 14:21:20 +0200

    Add new commands to command handler

--- src/main/scala/codecrafters_redis/Command.scala
+++ src/main/scala/codecrafters_redis/Command.scala
@@ -2,23 +2,57 @@ package codecrafters_redis
 
 class Command {
 
-  def echoCommand(input: String): String = {
-    input
+  def commandCommand(): String = {
+    "OK"
   }
 
   def pingCommand(): String = {
     "PONG"
   }
+  
+  def echoCommand(input: List[String]): String = {
+    if (input.length > 1) {
+      throw new RuntimeException(s"ECHO command expects 1 argument but received ${input.length}.")
+    }
 
-  def execute(input: List[String]): String = {
+    input.head
+  }
+
+  def setCommand(input: List[String], storage: DataStorage): String = {
+    if (input.length != 2) {
+      throw new RuntimeException(s"SET command expects exactly 2 arguments but received ${input.length}.")
+    }
+
+    val key = input.head
+    val value = input.last
+
+    storage.setValue(key, value)
+
+    "OK"
+  }
+
+  def getCommand(input: List[String], storage: DataStorage): String = {
+    if (input.length != 1) {
+      throw new RuntimeException(s"GET command expects exactly 1 argument but received ${input.length}.")
+    }
+
+    val key = input.head
+
+    storage.getValue(key)
+  }
+
+  def execute(input: List[String], storage: DataStorage): String = {
     if (input.isEmpty) throw new Exception("No command received")
 
-    val command = input.head.toUpperCase
-    val commandInput = input.tail.headOption.getOrElse("")
+    val command = input.headOption.getOrElse(throw new Exception("No command received")).toUpperCase()
+    val commandInput = input.slice(1, input.length)
 
     command match {
+      case "COMMAND" => commandCommand()
       case "PING" => pingCommand()
       case "ECHO" => echoCommand(commandInput)
+      case "SET" => setCommand(commandInput, storage)
+      case "GET" => getCommand(commandInput, storage)
       case other  => throw new Exception(s"Command $other not recognised.")
     }
   }
