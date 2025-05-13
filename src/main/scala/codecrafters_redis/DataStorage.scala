9eea30ced5b7001514d6715a0d31a9e0d1409596
Trent Park <trentpark8800@gmail.com>
2025-05-13 09:41:44 +0200

    Add deletion and time to live functionality to the data storage class

--- src/main/scala/codecrafters_redis/DataStorage.scala
+++ src/main/scala/codecrafters_redis/DataStorage.scala
@@ -1,20 +1,47 @@
 package codecrafters_redis
 
 trait DataStorage {
-  def setValue(key: String, value: String): Unit
-  def getValue(key: String): String
+  def setValue(key: String, value: String, ttl: Option[Long]): Unit
+  def getValue(key: String): Option[String]
+  def deleteValue(key: String): Unit
+  def checkExpiry(key: String): Unit
 }
 
 object InMemoryDataStorage extends DataStorage {
   
-  val mapMut = scala.collection.mutable.Map[String, String]()
+  val cache = scala.collection.mutable.Map[String, String]()
+  val expiryCache = scala.collection.mutable.Map[String, Long]()
 
-  def setValue(key: String, value: String): Unit = {
-    mapMut += (key -> value)
+  def setValue(key: String, value: String, ttl: Option[Long] = None): Unit = {
+
+    ttl.foreach { t =>
+      val expiryTime = System.currentTimeMillis() + t
+      expiryCache += (key -> expiryTime)
+    }
+    cache += (key -> value)
   }
 
-  def getValue(key: String): String = {
-    mapMut(key)
+  def getValue(key: String): Option[String] = {
+  
+    checkExpiry(key)
+    cache.get(key)
+  }
+
+  def deleteValue(key: String): Unit = {
+    cache -= (key)
+    expiryCache -= (key)
+  }
+
+  def checkExpiry(key: String): Unit = {
+
+    if (expiryCache.contains(key)) {
+      val currentTime = System.currentTimeMillis()
+      val expiryCandidateTime = expiryCache(key)
+
+      if (expiryCandidateTime <= currentTime) {
+        deleteValue(key)
+      }
+    }
   }
 
 }
