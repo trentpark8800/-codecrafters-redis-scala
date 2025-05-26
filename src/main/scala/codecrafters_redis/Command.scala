66083adb38b9b2e97f1ba6ac1343aca8ac063e0c
Trent Park <trentpark8800@gmail.com>
2025-05-26 17:36:29 +0200

    Add RespReply type to Command to handle varying reply types

--- src/main/scala/codecrafters_redis/Command.scala
+++ src/main/scala/codecrafters_redis/Command.scala
@@ -1,24 +1,33 @@
 package codecrafters_redis
 
+trait RespReply
+
+case class RespSimpleString(value: String) extends RespReply
+case class RespError(message: String) extends RespReply
+case class RespInteger(value: Integer) extends RespReply
+case class RespBulkString(value: String) extends RespReply
+case class RespArray(value: List[RespReply]) extends RespReply
+case object RespNull extends RespReply
+
 class Command {
 
-  def commandCommand(): Option[String] = {
-    Some("OK")
+  def commandCommand(): RespReply = {
+    RespSimpleString("OK")
   }
 
-  def pingCommand(): Option[String] = {
-    Some("PONG")
+  def pingCommand(): RespReply = {
+    RespSimpleString("PONG")
   }
   
-  def echoCommand(input: List[String]): Option[String] = {
+  def echoCommand(input: List[String]): RespReply = {
     if (input.length > 1) {
       throw new RuntimeException(s"ECHO command expects 1 argument but received ${input.length}.")
     }
 
-    Some(input.head)
+    RespSimpleString(input.head)
   }
 
-  def setCommand(input: List[String], storage: DataStorage): Option[String] = {
+  def setCommand(input: List[String], storage: DataStorage): RespReply = {
     if (input.length > 4) {
       throw new RuntimeException(s"SET command expects up to 4 arguments but received ${input.length}.")
     }
@@ -37,20 +46,43 @@ class Command {
         storage.setValue(key, value, None)
     }
 
-    Some("OK")
+    RespSimpleString("OK")
   }
 
-  def getCommand(input: List[String], storage: DataStorage): Option[String] = {
+  def getCommand(input: List[String], storage: DataStorage): RespReply = {
     if (input.length != 1) {
       throw new RuntimeException(s"GET command expects exactly 1 argument but received ${input.length}.")
     }
 
     val key = input.head
 
-    storage.getValue(key)
+    val value = storage.getValue(key)
+
+    value match {
+      case Some(v) => RespSimpleString(v)
+      case None => RespNull
+    }
   }
 
-  def execute(input: List[String], storage: DataStorage): Option[String] = {
+  def configCommand(input: List[String], config: Config): RespReply = {
+
+    if (input.length != 2) {
+      throw new RuntimeException(s"CONFIG command requires a sub-command and argument.")
+    }
+
+    val subCommand = input.head.toUpperCase()
+    val subarg = input(1).toLowerCase()
+
+    subCommand match {
+      case "GET" =>
+          config.getValue(subarg) match {
+            case Some(value) => RespArray(List(RespSimpleString(subarg), RespSimpleString(value)))
+            case None => RespNull
+          }
+    }
+  }
+
+  def execute(input: List[String], storage: DataStorage, config: Config): RespReply = {
     if (input.isEmpty) throw new Exception("No command received")
 
     val command = input.headOption.getOrElse(throw new Exception("No command received")).toUpperCase()
@@ -62,6 +94,7 @@ class Command {
       case "ECHO" => echoCommand(commandInput)
       case "SET" => setCommand(commandInput, storage)
       case "GET" => getCommand(commandInput, storage)
+      case "CONFIG" => configCommand(commandInput, config)
       case other  => throw new Exception(s"Command $other not recognised.")
     }
   }
